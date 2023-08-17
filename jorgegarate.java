1ab1fdc6ff708fa11d648ced61c407ffca265bd4
jorge garate <jorgeluis.garate@gmail.com>
2023-08-16 23:29:18 -0400

    Curso JAVA en coder

---
+++ jorgegarate.java
@@ -0,0 +1,15 @@
+import java.util.function.Function;
+
+class main {
+
+public static void main(String[] arg) {
+    int numberInt = 4 ;
+    float numberFloat = numberInt;
+    System.out.println("El numero entero es "+ numberInt);
+    System.out.println("El numero Float es "+ numberFloat);
+
+
+    
+}
+
+}
